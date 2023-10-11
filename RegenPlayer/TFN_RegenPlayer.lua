778f12a1f7f8fd8c9983806f42f0b46565c13615
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-10-11 18:06:46 +0200

    Update TFN_RegenPlayer.lua

--- RegenPlayer/TFN_RegenPlayer.lua
+++ RegenPlayer/TFN_RegenPlayer.lua
@@ -1,26 +1,39 @@
 --[[
 TFN_RegenPlayer by Rickoff for Tales from Nirn server
-tes3mp 0.7.0
-openmw 0.44
+tes3mp 0.8.1
 ---------------------------
 DESCRIPTION :
 natural regen health for players
 ---------------------------
 INSTALLATION:
 Save the file as TFN_RegenPlayer.lua inside your server/scripts/custom folder.
-
 Edits to customScripts.lua
 TFN_RegenPlayer = require("custom.TFN_RegenPlayer")
 ]]
 local tabSpell = {"natural_fortify_attack", "natural_fortify_speed", "natural_regen_health", "natural_regen_stamina", "natural_regen_mana"}
 
+local function AddSpell(pid, listSpell)
+	local Change = false
+	tes3mp.ClearSpellbookChanges(pid)
+	tes3mp.SetSpellbookChangesAction(pid, enumerations.spellbook.ADD)
+	for _, spellId in ipairs(listSpell) do
+		if not tableHelper.containsValue(Players[pid].data.spellbook, spellId) then	
+			tes3mp.AddSpell(pid, spellId)
+			table.insert(Players[pid].data.spellbook, spellId)
+			Change = true	
+		end	
+	end
+	if Change then
+		tes3mp.SendSpellbookChanges(pid)		
+	end
+end
+
 local TFN_RegenPlayer = {}
 
 TFN_RegenPlayer.OnServerInit = function(eventStatus)	
 	local spellCustom = jsonInterface.load("recordstore/spell.json")
-	local recordStore = RecordStores["spell"]
-	
-	if not tableHelper.containsValue(spellCustom.permanentRecords, "natural_fortify_attack") then
+	local recordStore = RecordStores["spell"]	
+	if not recordStore.data.permanentRecords["natural_fortify_attack"] then
 		local recordTable = {
 		  name = "Fortify Attack",
 		  subtype = 1,
@@ -38,9 +51,8 @@ TFN_RegenPlayer.OnServerInit = function(eventStatus)
 			}}
 		}
 		recordStore.data.permanentRecords["natural_fortify_attack"] = recordTable
-	end	
-
-	if not tableHelper.containsValue(spellCustom.permanentRecords, "natural_fortify_speed") then
+	end
+	if not recordStore.data.permanentRecords["natural_fortify_speed"] then
 		local recordTable = {
 		  name = "Fortify Speed",
 		  subtype = 1,
@@ -58,9 +70,8 @@ TFN_RegenPlayer.OnServerInit = function(eventStatus)
 			}}
 		}
 		recordStore.data.permanentRecords["natural_fortify_speed"] = recordTable
-	end
-	
-	if not tableHelper.containsValue(spellCustom.permanentRecords, "natural_regen_health") then
+	end	
+	if not recordStore.data.permanentRecords["natural_regen_health"] then
 		local recordTable = {
 		  name = "Health Regen",
 		  subtype = 1,
@@ -79,7 +90,7 @@ TFN_RegenPlayer.OnServerInit = function(eventStatus)
 		}
 		recordStore.data.permanentRecords["natural_regen_health"] = recordTable
 	end	
-	if not tableHelper.containsValue(spellCustom.permanentRecords, "natural_regen_stamina") then
+	if not recordStore.data.permanentRecords["natural_regen_stamina"] then
 		local recordTable = {
 		  name = "Stamina Regen",
 		  subtype = 1,
@@ -97,8 +108,7 @@ TFN_RegenPlayer.OnServerInit = function(eventStatus)
 			}}
 		}
 		recordStore.data.permanentRecords["natural_regen_stamina"] = recordTable
-	end		
-	
+	end	
 	if not tableHelper.containsValue(spellCustom.permanentRecords, "natural_regen_mana") then
 		local recordTable = {
 		  name = "Mana Regen",
@@ -118,17 +128,11 @@ TFN_RegenPlayer.OnServerInit = function(eventStatus)
 		}
 		recordStore.data.permanentRecords["natural_regen_mana"] = recordTable
 	end		
-    recordStore:Save()		
+	recordStore:Save()		
 end	
 
 TFN_RegenPlayer.OnPlayerAuthentified = function(eventStatus, pid)
-    if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		for slot, spell in pairs(tabSpell) do
-			if not tableHelper.containsValue(Players[pid].data.spellbook, spell, true) then	
-				logicHandler.RunConsoleCommandOnPlayer(pid, "player->addspell "..spell, false)		
-			end
-		end
-	end
+	AddSpell(pid, tabSpell)
 end
 
 customEventHooks.registerHandler("OnServerInit", TFN_RegenPlayer.OnServerInit)
