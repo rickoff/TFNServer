8d35ed1d0b528080359e666885e03ee7d683fe30
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-10-11 07:11:39 +0200

    Update TFN_ActivatePlayer.lua

--- ActivePlayer/TFN_ActivatePlayer.lua
+++ ActivePlayer/TFN_ActivatePlayer.lua
@@ -1,149 +1,150 @@
 --[[
 TFN_ActivatePlayer by Rickoff
-tes3mp 0.7.0
+tes3mp 0.8.1
 ---------------------------
 DESCRIPTION :
 Active player for resurrect
 ---------------------------
 INSTALLATION:
 Save the file as TFN_ActivatePlayer.lua inside your server/scripts/custom folder.
-Save the file as MenuActive.lua inside your server/scripts/menu folder.
 Edits to customScripts.lua :
 TFN_ActivatePlayer = require("custom.TFN_ActivatePlayer")
-Edits to config.lua :
-add in config.menuHelperFiles, "MenuActive"
+---------------------------
+USE:
+/res in chat for open menu resurrection
 ]]
-local config = {}
-config.pourcent = 25
+local cfg = {
+	pourcent = 25
+}
+
+local gui = {
+	ResurectGUI = 10102023,
+	ResurectTargetGUI = 11102023
+}
+
+local function GetName(pid)
+	return string.lower(Players[pid].accountName)
+end
+
+local function ResurrectProcess(pid)	
+	local currentResurrectType
+	if config.respawnAtImperialShrine == true then
+		if config.respawnAtTribunalTemple == true then
+			if math.random() > 0.5 then
+				currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
+			else
+				currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
+			end
+		else
+			currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
+		end
+	elseif config.respawnAtTribunalTemple == true then
+		currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
+	elseif config.defaultRespawnCell ~= nil then
+		currentResurrectType = enumerations.resurrect.REGULAR
+		tes3mp.SetCell(pid, config.defaultRespawnCell)
+		tes3mp.SendCell(pid)
+		if config.defaultRespawnPos ~= nil and config.defaultRespawnRot ~= nil then
+			tes3mp.SetPos(pid, config.defaultRespawnPos[1], config.defaultRespawnPos[2], config.defaultRespawnPos[3])
+			tes3mp.SetRot(pid, config.defaultRespawnRot[1], config.defaultRespawnRot[2])
+			tes3mp.SendPos(pid)
+		end
+	end
+	local message = "You have been revived"
+	if currentResurrectType == enumerations.resurrect.IMPERIAL_SHRINE then
+		message = message .. " at the nearest shrine"
+	elseif currentResurrectType == enumerations.resurrect.TRIBUNAL_TEMPLE then
+		message = message .. " to the nearest temple"
+	end
+	message = message .. ".\n"
+	if Players[pid].data.shapeshift.isWerewolf == true then
+		Players[pid]:SetWerewolfState(false)
+	end
+	contentFixer.UnequipDeadlyItems(pid)
+	tes3mp.Resurrect(pid, currentResurrectType)
+	tes3mp.SendMessage(pid, message, false)
+end
+
+local function ResurrectPlayer(pid)
+	tes3mp.Resurrect(tonumber(pid), 0)
+	local PlayerHealthBase = tes3mp.GetHealthBase(pid)
+	local Pourcent = (PlayerHealthBase * cfg.pourcent) / 100
+	tes3mp.SetHealthCurrent(pid, Pourcent)
+	tes3mp.SendStatsDynamic(pid)
+end
+
+local function ShowResurectGUI(pid)
+	local message = (
+		color.Red.."You are unconcious.\n"..
+		color.White.."You can wait for another player\n"..
+		color.White.."or respawn at the nearest temple."
+	)
+	local bouton = "Respawn;Wait"
+	tes3mp.CustomMessageBox(pid, gui.ResurectGUI, message, bouton)
+end
+
+local function ShowResurectTargetGUI(pid, targetPid)
+	local targetName = GetName(targetPid)
+	local message = (
+		color.White.."Do you want to "..
+		color.Gold.."help\n"..
+		color.White..targetName.." ?"
+	)
+	local bouton = "Patch them up;Leave them there"
+	tes3mp.CustomMessageBox(pid, gui.ResurectTargetGUI, message, bouton)
+end
 
 local TFN_ActivatePlayer = {}
 
-TFN_ActivatePlayer.ResurrectPlayer = function(pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		tes3mp.Resurrect(tonumber(pid), 0)
-		local PlayerHealthBase = tes3mp.GetHealthBase(pid)
-		local Pourcent = (PlayerHealthBase * config.pourcent) / 100
-		tes3mp.SetHealthCurrent(pid, Pourcent)
-		tes3mp.SendStatsDynamic(pid)
-	end
-end
-
-TFN_ActivatePlayer.OnCheckStatePlayer = function(eventStatus, pid, cellDescription, objects, players)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		local PlayerPid
-		for _, object in pairs(players) do
-			PlayerPid = object.pid
-		end	
-		if Players[PlayerPid] ~= nil and Players[PlayerPid]:IsLoggedIn() then		
-			Players[pid].data.targetPid = PlayerPid	
-			Players[PlayerPid].data.targetPid = pid			
-			local PlayerActivatedHealth = tes3mp.GetHealthCurrent(PlayerPid)	
-			if PlayerActivatedHealth <= 0 then
-				Players[pid].currentCustomMenu = "resurrect player"--Menu Resurrect
-				menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)				
-			end
+TFN_ActivatePlayer.OnObjectActivate = function(eventStatus, pid, cellDescription, objects, players)	
+	local TargetPid
+	for _, object in pairs(players) do
+		TargetPid = object.pid
+	end	
+	if Players[TargetPid] and Players[TargetPid]:IsLoggedIn() then		
+		Players[pid].data.targetPid = TargetPid	
+		Players[TargetPid].data.targetPid = pid			
+		local PlayerActivatedHealth = tes3mp.GetHealthCurrent(TargetPid)	
+		if PlayerActivatedHealth <= 0 then
+			ShowResurectTargetGUI(pid, TargetPid)
 		end
 	end
 end
 
-TFN_ActivatePlayer.OnDeathTime = function(eventStatus, pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then 
-		return customEventHooks.makeEventStatus(false,true)
-	end
+TFN_ActivatePlayer.OnDeathTimeExpirationValidator = function(eventStatus, pid)
+	return customEventHooks.makeEventStatus(false,true)
 end
 
-TFN_ActivatePlayer.OnDeathTimeExpiration = function(eventStatus, pid)
+TFN_ActivatePlayer.OnDeathTimeExpirationHandler = function(eventStatus, pid)
 	if eventStatus.validCustomHandlers then
-		local state = false
-		if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-			for slot, k in pairs(Players[pid].data.spellbook) do
-				if Players[pid].data.spellbook[slot] == "vampire sun damage" then
-					state = true
-					break
-				end
-			end	
-			if state == false then
-				Players[pid].currentCustomMenu = "resurrect"--Menu Resurrect
-				menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)	
-			else 
-				Players[pid].currentCustomMenu = "resurrectvamp"--Menu Resurrect Vamp
-				menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)	
-			end
-		end
-	end
-end
-
-TFN_ActivatePlayer.ResVamp = function(pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		contentFixer.UnequipDeadlyItems(pid)		
-		tes3mp.SetCell(pid, "Balmora, temple") 			
-		tes3mp.SetPos(pid, 3664, 4256, 14816)		
-		tes3mp.SendCell(pid)    
-		tes3mp.SendPos(pid)	
-		tes3mp.Resurrect(pid,0)
+		ShowResurectGUI(pid)
 	end
 end		
 
-TFN_ActivatePlayer.Res = function(pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		Players[pid]:Resurrect()
+TFN_ActivatePlayer.CommandRes = function(pid)
+	local PlayerActivatedHealth = tes3mp.GetHealthCurrent(pid)	
+	if PlayerActivatedHealth <= 0 then
+		ShowResurectGUI(pid)
 	end
 end
 
-TFN_ActivatePlayer.ResurrectCheck = function(eventStatus, pid)
-	TFN_ActivatePlayer.ResurrectProcess(pid)
-	return customEventHooks.makeEventStatus(false,false)	
-end	
-
-TFN_ActivatePlayer.ResurrectProcess = function(pid)	
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		if DragonDoor then
-			DragonDoor.OnPlayerConnect(true, pid)
-		end	
-		local currentResurrectType
-		if config.respawnAtImperialShrine == true then
-			if config.respawnAtTribunalTemple == true then
-				if math.random() > 0.5 then
-					currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
-				else
-					currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
-				end
-			else
-				currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
-			end
-		elseif config.respawnAtTribunalTemple == true then
-			currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
-		elseif config.defaultRespawnCell ~= nil then
-			currentResurrectType = enumerations.resurrect.REGULAR
-			tes3mp.SetCell(pid, config.defaultRespawnCell)
-			tes3mp.SendCell(pid)
-			if config.defaultRespawnPos ~= nil and config.defaultRespawnRot ~= nil then
-				tes3mp.SetPos(pid, config.defaultRespawnPos[1], config.defaultRespawnPos[2], config.defaultRespawnPos[3])
-				tes3mp.SetRot(pid, config.defaultRespawnRot[1], config.defaultRespawnRot[2])
-				tes3mp.SendPos(pid)
-			end
+TFN_ActivatePlayer.OnGuiMenu = function(eventStatus, pid, idGui, data)
+	if idGui == gui.ResurectGUI then		
+		if tonumber(data) == 0 then
+			ResurrectProcess(pid)			
 		end
-		local message = "You have been revived"
-		if currentResurrectType == enumerations.resurrect.IMPERIAL_SHRINE then
-			message = message .. " at the nearest shrine"
-		elseif currentResurrectType == enumerations.resurrect.TRIBUNAL_TEMPLE then
-			message = message .. " to the nearest temple"
+	end	
+	if idGui == gui.ResurectTargetGUI then		
+		if tonumber(data) == 0 then
+			ResurrectPlayer(Players[pid].data.targetPid)
 		end
-		message = message .. ".\n"
-		if Players[pid].data.shapeshift.isWerewolf == true then
-			Players[pid]:SetWerewolfState(false)
-		end
-		contentFixer.UnequipDeadlyItems(pid)
-		tes3mp.Resurrect(pid, currentResurrectType)
-		tes3mp.SendMessage(pid, message, false)
-	end
+	end	
 end
 
-customEventHooks.registerHandler("OnObjectActivate", TFN_ActivatePlayer.OnCheckStatePlayer)
-customEventHooks.registerValidator("OnPlayerResurrect", TFN_ActivatePlayer.ResurrectCheck)
-customEventHooks.registerValidator("OnDeathTimeExpiration", TFN_ActivatePlayer.OnDeathTime)
-customEventHooks.registerHandler("OnDeathTimeExpiration", TFN_ActivatePlayer.OnDeathTimeExpiration)
-customCommandHooks.registerCommand("resurrectvamp", TFN_ActivatePlayer.ResVamp)
-customCommandHooks.registerCommand("resurrect", TFN_ActivatePlayer.Res)
+customEventHooks.registerHandler("OnGUIAction", TFN_ActivatePlayer.OnGuiMenu)
+customEventHooks.registerHandler("OnObjectActivate", TFN_ActivatePlayer.OnObjectActivate)
+customEventHooks.registerValidator("OnDeathTimeExpiration", TFN_ActivatePlayer.OnDeathTimeExpirationValidator)
+customEventHooks.registerHandler("OnDeathTimeExpiration", TFN_ActivatePlayer.OnDeathTimeExpirationHandler)
+customCommandHooks.registerCommand("res", TFN_ActivatePlayer.CommandRes)
 
 return TFN_ActivatePlayer
