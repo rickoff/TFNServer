c2c026a06169005e3ea2433e3f6620cb3e91a9f7
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-10-11 18:15:37 +0200

    Update TFN_LevelingPlayer.lua

--- LevelingPlayer/TFN_LevelingPlayer.lua
+++ LevelingPlayer/TFN_LevelingPlayer.lua
@@ -1,8 +1,7 @@
 --[[
 TFN_LevelingPlayer by Rickoff for Tales from Nirn server 
-tes3mp 0.7.0
-openmw 0.44
-script 0.1
+tes3mp 0.8.1
+script 0.2
 ---------------------------
 DESCRIPTION :
 Leveling sytem
@@ -23,186 +22,164 @@ FUNCTION:
 --MAIN CONFIG
 -- ===========
 -------------------------
-local trad = {}
-trad.Feli = "You have gained a level, congratulation : "
-trad.Menu = "/level"
-trad.Dep = " to spend your points"
-trad.Xps = " skill."
-trad.NotPts = "You don't have enough skill points to decrease"
-trad.NoPt =  "You don't have enough skill points, current : "
-trad.Req = " required : "
-trad.WereWolf = "Skill menu is forbidden in werewolf form"
+local trad = {
+	Feli = "You have gained a level, congratulation : ",
+	Menu = "/level",
+	Dep = " to spend your points",
+	Xps = " skill.",
+	NotPts = "You don't have enough skill points to decrease",
+	NoPt =  "You don't have enough skill points, current : ",
+	Req = " required : ",
+	WereWolf = "Skill menu is forbidden in werewolf form"
+}
 
-local config = {}
-config.levelMax = 50
-config.LevelHealth = 10
-config.talentProgress = 10
-config.RewardPoints = 10
-config.PlayerAddPoint = 555555
+local cfg = {
+	levelMax = 50,
+	LevelHealth = 10,
+	talentProgress = 10,
+	RewardPoints = 10,
+	PlayerAddPoint = 555555
+}
 
 local PlayerOptionPoint = {}
-
-local TFN_LevelingPlayer = {}
 
 local function getName(pid)
 	return string.lower(Players[pid].accountName)
 end
 
+local TFN_LevelingPlayer = {}
+
 TFN_LevelingPlayer.InputDialog = function(pid, comp, state)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then 
-		PlayerOptionPoint[getName(pid)] = {comp = comp, state = state}
-		local message = "Enter the number of points"
-		if state == "Add" then
-			return tes3mp.InputDialog(pid, config.PlayerAddPoint, message, "add skill points")
-		elseif state == "Remove" then
-			return tes3mp.InputDialog(pid, config.PlayerAddPoint, message, "remove skill points")
-		end	
+	PlayerOptionPoint[getName(pid)] = {comp = comp, state = state}
+	local message = "Enter the number of points"
+	if state == "Add" then
+		return tes3mp.InputDialog(pid, cfg.PlayerAddPoint, message, "add skill points")
+	elseif state == "Remove" then
+		return tes3mp.InputDialog(pid, cfg.PlayerAddPoint, message, "remove skill points")
 	end
 end
 
 TFN_LevelingPlayer.OnGUIAction = function(pid, idGui, data)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then 
-		if idGui == config.PlayerAddPoint then
-			TFN_LevelingPlayer.OnPlayerCompetence(pid, PlayerOptionPoint[getName(pid)].comp, PlayerOptionPoint[getName(pid)].state, tonumber(data))
-			Players[pid].currentCustomMenu = "menu leveling"
-			return true, menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)			
-		end
+	if idGui == cfg.PlayerAddPoint then
+		TFN_LevelingPlayer.OnPlayerCompetence(pid, PlayerOptionPoint[getName(pid)].comp, PlayerOptionPoint[getName(pid)].state, tonumber(data))
+		Players[pid].currentCustomMenu = "menu leveling"
+		return true, menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)			
 	end
 end	 
 
-TFN_LevelingPlayer.OnPlayerCompetence = function(pid, Comp, State, Count)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		local PointCount = Players[pid].data.customVariables.TfnLeveling.pointSoul	
-		local Count = Count or 0
-		local Check = "Nothing"
-		local Type = "Nothing"
-		
-		if Comp ~= nil then
-		
-			if Players[pid].data.skills[Comp] then
-				Type = "skill"
-			elseif Players[pid].data.attributes[Comp] then
-				Type = "attribute"
+TFN_LevelingPlayer.OnPlayerCompetence = function(pid, Comp, State, Count)	
+	local PointCount = Players[pid].data.customVariables.TfnLeveling.pointSoul	
+	local Count = Count or 0
+	local Check = "Nothing"
+	local Type = "Nothing"	
+	if Comp ~= nil then	
+		if Players[pid].data.skills[Comp] then
+			Type = "skill"
+		elseif Players[pid].data.attributes[Comp] then
+			Type = "attribute"
+		end		
+		if PointCount >= Count and State == "Add" then	
+			if Type == "skill" then
+				local skillId = tes3mp.GetSkillId(Comp)
+				local valueC = Players[pid].data.skills[Comp].base + Count
+				tes3mp.SetSkillBase(pid, skillId, valueC)
+				Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul - Count	
+				Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] + Count
+				Check = "skill"
+			else
+				local attrId = tes3mp.GetAttributeId(Comp)
+				local valueS = Players[pid].data.attributes[Comp].base + Count	
+				tes3mp.SetAttributeBase(pid, attrId, valueS)
+				Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul - Count
+				Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] + Count
+				Check = "attribute"
 			end
-			
-			if PointCount >= Count and State == "Add" then	
-				if Type == "skill" then
-					local skillId = tes3mp.GetSkillId(Comp)
-					local valueC = Players[pid].data.skills[Comp].base + Count
-					tes3mp.SetSkillBase(pid, skillId, valueC)
-					Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul - Count	
-					Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] + Count
-					Check = "skill"
-				else
-					local attrId = tes3mp.GetAttributeId(Comp)
-					local valueS = Players[pid].data.attributes[Comp].base + Count	
-					tes3mp.SetAttributeBase(pid, attrId, valueS)
-					Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul - Count
-					Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] + Count
-					Check = "attribute"
-				end
-			elseif PointCount < Count and State == "Add" then
-				tes3mp.MessageBox(pid, -1, color.Default..trad.NoPt..PointCount)
-			end	
-			
-			if Players[pid].data.customVariables.TfnLeveling[Comp] >= Count and State == "Remove" then	
-				if Type == "skill" then		
-					local skillId = tes3mp.GetSkillId(Comp)
-					local valueC = Players[pid].data.skills[Comp].base - Count
-					tes3mp.SetSkillBase(pid, skillId, valueC)
-					Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + Count	
-					Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] - Count
-					Check = "skill"
-				else
-					local attrId = tes3mp.GetAttributeId(Comp)
-					local valueS = Players[pid].data.attributes[Comp].base - Count	
-					tes3mp.SetAttributeBase(pid, attrId, valueS)
-					Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + Count
-					Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] - Count
-					Check = "attribute"				
-				end
-			elseif Players[pid].data.customVariables.TfnLeveling[Comp] < Count and State == "Remove" then
-				tes3mp.MessageBox(pid, -1, color.Default..trad.NotPts)
+		elseif PointCount < Count and State == "Add" then
+			tes3mp.MessageBox(pid, -1, color.Default..trad.NoPt..PointCount)
+		end		
+		if Players[pid].data.customVariables.TfnLeveling[Comp] >= Count and State == "Remove" then	
+			if Type == "skill" then		
+				local skillId = tes3mp.GetSkillId(Comp)
+				local valueC = Players[pid].data.skills[Comp].base - Count
+				tes3mp.SetSkillBase(pid, skillId, valueC)
+				Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + Count	
+				Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] - Count
+				Check = "skill"
+			else
+				local attrId = tes3mp.GetAttributeId(Comp)
+				local valueS = Players[pid].data.attributes[Comp].base - Count	
+				tes3mp.SetAttributeBase(pid, attrId, valueS)
+				Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + Count
+				Players[pid].data.customVariables.TfnLeveling[Comp] = Players[pid].data.customVariables.TfnLeveling[Comp] - Count
+				Check = "attribute"				
 			end
-			
-			if Check == "attribute" then
-				Players[pid]:SaveAttributes()	
-				tes3mp.SendAttributes(pid)	
-			elseif Check == "skill" then
-				Players[pid]:SaveSkills()			
-				tes3mp.SendSkills(pid)
-			end
+		elseif Players[pid].data.customVariables.TfnLeveling[Comp] < Count and State == "Remove" then
+			tes3mp.MessageBox(pid, -1, color.Default..trad.NotPts)
+		end		
+		if Check == "attribute" then
+			Players[pid]:SaveAttributes()	
+			tes3mp.SendAttributes(pid)	
+		elseif Check == "skill" then
+			Players[pid]:SaveSkills()			
+			tes3mp.SendSkills(pid)
 		end
 	end
 end	
 
 TFN_LevelingPlayer.MainMenu = function(pid)
-    if Players[pid]~= nil and Players[pid]:IsLoggedIn() then
-		if not tes3mp.IsWerewolf(pid) then
-			Players[pid].currentCustomMenu = "menu leveling"
-			menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)
-		else
-			tes3mp.MessageBox(pid, -1, trad.WereWolf)
-		end
+	if not tes3mp.IsWerewolf(pid) then
+		Players[pid].currentCustomMenu = "menu leveling"
+		menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)
+	else
+		tes3mp.MessageBox(pid, -1, trad.WereWolf)
 	end
 end
 
-TFN_LevelingPlayer.LevelPlayer = function(eventStatus, pid)	
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		if Players[pid].data.stats.level >= config.levelMax then
-			Players[pid].data.stats.level = config.levelMax
-			Players[pid].data.stats.levelProgress = 0
-			Players[pid]:LoadLevel()
-			return customEventHooks.makeEventStatus(false,false)				
-		end			
-		Players[pid]:QuicksaveToDrive()
+TFN_LevelingPlayer.OnPlayerLevelValidator = function(eventStatus, pid)	
+	if Players[pid].data.stats.level >= cfg.levelMax then
+		Players[pid].data.stats.level = cfg.levelMax
+		Players[pid].data.stats.levelProgress = 0
+		Players[pid]:LoadLevel()
+		return customEventHooks.makeEventStatus(false,false)				
 	end
 end
 
-TFN_LevelingPlayer.GetlevelSoul = function(eventStatus, pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		if Players[pid].data.stats.levelProgress >= config.talentProgress then			
-			Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + config.RewardPoints
-			TFN_LevelingPlayer.NewLevelPlayer(pid)
-			tes3mp.MessageBox(pid, -1, color.Default..trad.Feli..color.Green..trad.Menu..color.Default..trad.Dep..color.Yellow..trad.Xps)
-		end
+TFN_LevelingPlayer.OnPlayerLevelHandler = function(eventStatus, pid)
+	if Players[pid].data.stats.levelProgress >= cfg.talentProgress then			
+		Players[pid].data.customVariables.TfnLeveling.pointSoul = Players[pid].data.customVariables.TfnLeveling.pointSoul + cfg.RewardPoints
+		TFN_LevelingPlayer.NewLevelPlayer(pid)
+		tes3mp.MessageBox(pid, -1, color.Default..trad.Feli..color.Green..trad.Menu..color.Default..trad.Dep..color.Yellow..trad.Xps)
 	end
 end
 
 TFN_LevelingPlayer.NewLevelPlayer = function(pid)	
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		if Players[pid].data.stats.level < config.levelMax then
-			Players[pid].data.stats.level = Players[pid].data.stats.level + 1
-			Players[pid].data.stats.levelProgress = 0 
-			tes3mp.SetHealthBase(pid, (Players[pid].data.stats.healthBase + config.LevelHealth)) 
-			Players[pid]:LoadLevel()
-			Players[pid]:SaveStatsDynamic()
-			tes3mp.SendStatsDynamic(pid)
-		end
-		Players[pid]:QuicksaveToDrive()
+	if Players[pid].data.stats.level < cfg.levelMax then
+		Players[pid].data.stats.level = Players[pid].data.stats.level + 1
+		Players[pid].data.stats.levelProgress = 0 
+		tes3mp.SetHealthBase(pid, (Players[pid].data.stats.healthBase + cfg.LevelHealth)) 
+		Players[pid]:LoadLevel()
+		Players[pid]:SaveStatsDynamic()
+		tes3mp.SendStatsDynamic(pid)
 	end
 end
 
-TFN_LevelingPlayer.OnPlayerAddCustom = function(eventStatus, pid) 
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		if not Players[pid].data.customVariables.TfnLeveling then
-			Players[pid].data.customVariables.TfnLeveling = {}
-			Players[pid].data.customVariables.TfnLeveling.pointSoul = 0	
-			for attribute, slot in pairs(Players[pid].data.attributes) do
-				Players[pid].data.customVariables.TfnLeveling[attribute] = 0
-			end
-			for skill, slot in pairs(Players[pid].data.skills) do
-				Players[pid].data.customVariables.TfnLeveling[skill] = 0
-			end				
-			Players[pid]:QuicksaveToDrive()			
-		end		
+TFN_LevelingPlayer.OnPlayerAuthentified = function(eventStatus, pid)
+	if not Players[pid].data.customVariables.TfnLeveling then
+		Players[pid].data.customVariables.TfnLeveling = {}
+		Players[pid].data.customVariables.TfnLeveling.pointSoul = 0	
+		for attribute, slot in pairs(Players[pid].data.attributes) do
+			Players[pid].data.customVariables.TfnLeveling[attribute] = 0
+		end
+		for skill, slot in pairs(Players[pid].data.skills) do
+			Players[pid].data.customVariables.TfnLeveling[skill] = 0
+		end			
 	end
 end
 
 customCommandHooks.registerCommand("level", TFN_LevelingPlayer.MainMenu)
-customEventHooks.registerValidator("OnPlayerLevel", TFN_LevelingPlayer.LevelPlayer)
-customEventHooks.registerHandler("OnPlayerLevel", TFN_LevelingPlayer.GetlevelSoul)
-customEventHooks.registerHandler("OnPlayerAuthentified", TFN_LevelingPlayer.OnPlayerAddCustom)
+customEventHooks.registerValidator("OnPlayerLevel", TFN_LevelingPlayer.OnPlayerLevelValidator)
+customEventHooks.registerHandler("OnPlayerLevel", TFN_LevelingPlayer.OnPlayerLevelHandler)
+customEventHooks.registerHandler("OnPlayerAuthentified", TFN_LevelingPlayer.OnPlayerAuthentified)
 customEventHooks.registerHandler("OnGUIAction", function(eventStatus, pid, idGui, data)
 	if TFN_LevelingPlayer.OnGUIAction(pid, idGui, data) then return end
 end)
